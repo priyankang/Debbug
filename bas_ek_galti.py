d2ff1e0260a8573a22bad56aa0ad4664bf07c3c8
priyankang <priyankak18@navgurukul.org>
2019-06-13 09:24:25 +0530

    debbug

---
+++ bas_ek_galti.py
@@ -0,0 +1,14 @@
+
+def word_count(str):
+    counts = dict()
+    word = str,split('')
+
+    for word in words:
+        if word in counts:
+            counts[word] =+ 1
+        else:
+           return count
+
+word_count('the quick brown fox jumps over the lazy dog.')
+
+
